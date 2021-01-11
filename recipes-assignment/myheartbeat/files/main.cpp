9121854e8e32b32c96e502901694864ae6331e6d
Giovanni <giovanni.paganini1995@gmail.com>
2021-01-11 17:45:58 +0100

    buffer size changed

--- recipes-assignment/myheartbeat/files/main.cpp
+++ recipes-assignment/myheartbeat/files/main.cpp
@@ -3,7 +3,7 @@
 #include "circular_buffer.hpp"
 
 int main() {
-    circular_buffer<unsigned int, 5> buffer;
+    circular_buffer<unsigned int, 4> buffer;
     producer p(20000, buffer);
     consumer c(buffer);
     return 0;
